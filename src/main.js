e3b6f9f57bf2215f389cbc484c26a7bf1cb462d6
Arcelys <egreezlya@gmail.com>
2025-04-11 22:36:39 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,37 @@
+import './style.css'
+import { excuses } from './data/excuses.js'
+
+const output = document.getElementById('excuse');
+const generateBtn = document.getElementById('generateBtn');
+const copyMsg = document.getElementById('copyMsg');
+const categorySelect = document.getElementById('category-select');
+
+Object.keys(excuses).forEach(cat => {
+  const option = document.createElement('option');
+  option.value = cat;
+  option.textContent = cat;
+  categorySelect.appendChild(option);
+});
+
+generateBtn.addEventListener('click', () => {
+  let category = categorySelect.value;
+  if (!category) return;
+
+  const list = excuses[category];
+  const excuse = list[Math.floor(Math.random() * list.length)]
+  
+  output.textContent = excuse;
+  output.classList.remove('hidden');
+  copyMsg.textContent = '';
+});
+
+excuseText.addEventListener('click', () => {
+  if (!output.textContent) return;
+  
+  navigator.clipboard.writeText(output.textContent)
+    .then(() => {copyMsg.textContent = `Скопированно: "${output.textContent}"`;
+    setTimeout(() => {
+      copyMsg.textContent = '';
+    }, 2000);
+  });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      'bgMain': '#6353f1',
+      'bgBtn': '#bab2ff',
+      'bgBtnHov': '#8274ff'
+    },
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
+
